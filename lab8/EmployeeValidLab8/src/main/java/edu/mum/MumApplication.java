7d512c291ce397cd3baddafb13ed81127321d94b
jorge <jorge_930614@hotmail.com>
2019-08-01 22:15:55 -0500

    Lab8

---
+++ lab8/EmployeeValidLab8/src/main/java/edu/mum/MumApplication.java
@@ -0,0 +1,13 @@
+package edu.mum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MumApplication.class, args);
+    }
+
+}
